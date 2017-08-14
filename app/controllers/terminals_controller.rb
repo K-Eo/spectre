f80b98b41bd2a11189d930df32f503bf5778d9f5
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-14 12:10:46 -0500

    Add test for terminal controller create, index, new

--- app/controllers/terminals_controller.rb
+++ app/controllers/terminals_controller.rb
@@ -8,4 +8,20 @@ class TerminalsController < ApplicationController
     @terminal = Terminal.new
   end
 
+  def create
+    @terminal = Terminal.new(terminal_params)
+
+    if @terminal.save
+      redirect_to terminals_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def terminal_params
+      params.require(:terminal).permit(:name)
+    end
+
 end
--- test/controllers/terminals_controller_test.rb
+++ test/controllers/terminals_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class TerminalsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get terminals_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    get terminals_path
+    assert_response :success
+  end
+
+  test "should post create" do
+    assert_difference 'Terminal.count' do
+      post terminals_path, params: { terminal: { name: 'foobar' } }
+      assert_redirected_to terminals_path
+    end
+  end
+
 end
