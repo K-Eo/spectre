7013edfccae4c6b346cc8fe1f8a108f6deba9129
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-14 17:42:46 -0500

    Add item tab helper

--- app/helpers/terminals_helper.rb
+++ app/helpers/terminals_helper.rb
@@ -1,2 +1,11 @@
 module TerminalsHelper
+
+  def tab_item(name, url)
+    content_tag :li, class: 'nav-item' do
+      klass = 'nav-link'
+      klass << ' active' if url.include?(controller_name)
+      concat link_to name, url, class: klass
+    end
+  end
+
 end
