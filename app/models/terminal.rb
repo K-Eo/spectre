315c9a166094b3e33062050d152a4f2d6aef66f5
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-15 00:00:21 -0500

    Generate pairing token before save terminal

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -3,4 +3,12 @@ class Terminal < ApplicationRecord
                     length: { maximum: 255 }
 
   default_scope { order(created_at: :desc, name: :asc) }
+
+  before_create :create_pairing_token
+
+  private
+
+    def create_pairing_token
+      self.pairing_token = SecureRandom.hex(10)
+    end
 end
