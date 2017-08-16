d846cecce1e983c48210592a91664289c2827cec
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-15 22:27:09 -0500

    Add preview pairing token mailer

--- test/mailers/previews/terminal_mailer_preview.rb
+++ test/mailers/previews/terminal_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/terminal_mailer
 class TerminalMailerPreview < ActionMailer::Preview
 
+  def pairing_token
+    terminal = Terminal.first
+    TerminalMailer.pairing_token('foo@bar.com', terminal)
+  end
+
 end
