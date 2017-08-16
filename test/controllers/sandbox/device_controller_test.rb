ee8b8677c91d557c3a2e10a2473ed93c6ed47973
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-16 16:28:43 -0500

    Add test for devices index

--- test/controllers/sandbox/device_controller_test.rb
+++ test/controllers/sandbox/device_controller_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class Sandbox::DeviceControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get index" do
+    get sandbox_devices_path
+    assert_response :success
+  end
+
 end
