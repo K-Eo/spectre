8fd24a671402c3faf66cf2f0bd61e2c44c5eec78
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-15 22:23:52 -0500

    Attach qr code to email

--- app/mailers/terminal_mailer.rb
+++ app/mailers/terminal_mailer.rb
@@ -1,9 +1,10 @@
 class TerminalMailer < ApplicationMailer
   default from: 'spectre@support.com'
 
-  def pairing_token(email, qrcode)
+  def pairing_token(email, terminal)
     @email = email
-    @qrcode = qrcode
+    @qrcode = terminal.pairing_token
+    attachments.inline['qrcode.svg'] = terminal.generate_pairing_token_qr
     mail(to: @email, subject: 'Instrucciones para asociar dispositivo')
   end
 
