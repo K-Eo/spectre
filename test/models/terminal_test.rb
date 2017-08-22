2eafbb864424ba1d8a44c9f1386078ce1cfdeb0e
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-22 18:12:50 -0500

    Fix terminal model tests

--- test/models/terminal_test.rb
+++ test/models/terminal_test.rb
@@ -2,13 +2,17 @@ require 'test_helper'
 
 class TerminalTest < ActiveSupport::TestCase
 
+  def setup
+    @tenant = tenants(:spectre)
+  end
+
   test "should be invalid" do
     terminal = Terminal.new
     assert_not terminal.valid?
   end
 
   test "name should be less than 255" do
-    terminal = Terminal.new
+    terminal = Terminal.new(tenant_id: @tenant.id)
     terminal.name = "1" * 256
     assert_not terminal.valid?
     terminal.name = "1" * 255
@@ -17,6 +21,7 @@ class TerminalTest < ActiveSupport::TestCase
 
   test "should generate tokens after create" do
     terminal = Terminal.new(name: 'foobar')
+    terminal.tenant_id = @tenant.id
     terminal.save
     terminal.reload
     assert_match /[a-zA-Z0-9]{24}/, terminal.pairing_token
