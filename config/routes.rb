50881c57c41b3ca21d88aab7bacf02f3c27b74db
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-22 12:19:11 -0500

    Add scope to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :terminals do
-    member do
-      post 'send_token'
-      # Unpair device from web
-      delete 'pair_device', to:'terminals#unpair_device_web'
+
+  # Scope routes for domain
+  scope(path: ':subdomain', constraints: { subdomain: /[\w\-]+/ }) do
+
+    resources :terminals do
+      member do
+        post 'send_token'
+        # Unpair device from web
+        delete 'pair_device', to:'terminals#unpair_device_web'
+      end
+      collection do
+        post 'pair_device', to: 'terminals#pair_device'
+        # Unpair device from API
+        delete 'pair_device', to: 'terminals#unpair_device'
+      end
     end
-    collection do
-      post 'pair_device', to: 'terminals#pair_device'
-      # Unpair device from API
-      delete 'pair_device', to: 'terminals#unpair_device'
-    end
+
   end
 
   root 'pages#index'
