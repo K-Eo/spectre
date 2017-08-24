5a9fb285a89d70036bf64b3273e1d820d5160a82
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-24 14:26:05 -0500

    Add tenant validations

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,9 @@
 class Tenant < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true,
+                    length: { maximum: 255 }
+  validates :organization, presence: true,
+                            length: { maximum: 50 },
+                            uniqueness: true
 end
