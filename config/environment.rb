696b7e43cc73d025ab7249525211f84f06d96175
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-25 17:29:51 -0500

    Add custom field error proc [skip ci]

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,14 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  html_tag.html_safe
+  if html_tag =~ /<(input|textarea|select)/
+    html_field = Nokogiri::HTML::DocumentFragment.parse(html_tag)
+    html_field.children.add_class 'is-invalid'
+    input_name = html_field.children.first[:id].split('_').last
+    errors = instance.object.errors[input_name].join('<br>')
+    html_field.children.first.add_next_sibling "<div class=\"invalid-feedback\">#{errors}</div>"
+    html_field.to_s.html_safe
+  else
+    html_tag.html_safe
+  end
 end
