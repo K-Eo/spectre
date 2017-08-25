b57dca81338958afaa13fd989689c98da64e2867
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-25 15:32:34 -0500

    Add helper to render flash mesasges

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+
+  def flash_message
+    html = ""
+    flash.each do |key, value|
+      type = flash_type(key)
+      html += <<-HTML
+        <div class="alert alert-#{type} alert-dismissable fade show" role="alert">
+          <div class="container">
+            <button class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            #{value}
+          </div>
+        </div>
+      HTML
+    end
+
+    html.html_safe
+  end
+
+private
+
+  def flash_type(type)
+    case type
+    when 'alert'
+      'danger'
+    when 'notice'
+      'success'
+    else
+      type
+    end
+  end
 end
