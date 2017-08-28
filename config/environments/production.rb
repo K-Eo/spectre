f868c9b19b16696958a8d241c254888980e112fc
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-28 14:19:33 -0500

    Fix dynamic host config [skip ci]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
+app_name = ENV['HEROKU_APP_NAME'] || 'spectre-55'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => "spectre-55.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "#{app_name}.herokuapp.com" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,3 @@
-app_name = ENV['HEROKU_APP_NAME'] || 'spectre'
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -13,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "#{app_name}@spectre.com"
+  config.mailer_sender = "spectre@spectre.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
