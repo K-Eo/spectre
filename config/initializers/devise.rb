653d7ba858f666473103954984aa9a435c5c138e
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-28 14:12:29 -0500

    Config dynamic devise mailer host [skip ci]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,4 @@
+app_name = ENV['HEROKU_APP_NAME'] || 'spectre'
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -12,7 +13,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'support@spectre.com'
+  config.mailer_sender = "#{app_name}@spectre.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
