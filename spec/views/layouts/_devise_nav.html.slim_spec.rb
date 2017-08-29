c28503cef0bc1be39d232d475f372cb11c290404
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-29 18:06:16 -0500

    Update _devise_nav partial

--- spec/views/layouts/_devise_nav.html.slim_spec.rb
+++ spec/views/layouts/_devise_nav.html.slim_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 describe 'layouts/_devise_nav' do
+  subject { rendered }
+
   before do
     render
   end
 
-  it 'renders nav wrapper' do
-    expect(rendered).to match(%{nav class="navbar navbar-expand-md navbar-dark px-0"})
+  context 'navbar' do
+    it { is_expected.to have_css('.navbar.navbar-expand-md.navbar-dark.px-0') }
+    it { is_expected.to have_css('nav#app-nav') }
   end
 
-  it 'renders brand link' do
-    expect(rendered).to match(%{<a class="navbar-brand" href="/">Spectre</a>})
+  context 'navbar brand' do
+    it { is_expected.to have_css('a.navbar-brand') }
+    it { is_expected.to have_link('Spectre', href: '/') }
+  end
+
+  context 'user links' do
+    it { is_expected.to have_css('div#user-links') }
   end
 end
