ceefba5e236a522e87c6810aed57859eccfc1550
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-28 21:25:37 -0500

    Add tests for application template

---
+++ spec/views/layouts/application.html.slim_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe 'layouts/application' do
+  context 'head' do
+    before do
+      render
+    end
+
+    context 'title' do
+      context 'when is given' do
+        it 'renders given name' do
+          assign(:title, 'Foobar')
+          render
+          expect(rendered).to match(%{<title>Foobar | Spectre</title>})
+        end
+      end
+
+      context 'when is not given' do
+        it 'renders only Spectre' do
+          expect(rendered).to match(%{<title>Spectre</title>})
+        end
+      end
+    end
+
+    it 'renders utf-8' do
+      expect(rendered).to match(%{charset="utf-8"})
+    end
+
+    it 'renders viewport meta' do
+      expect(rendered).to match(%{name="viewport"})
+    end
+
+    it 'renders bootstrap resposive settings' do
+      expect(rendered).to match(%{content="width=device-width, initial-scale=1, shrink-to-fit=no"})
+    end
+  end
+
+  context 'body' do
+    it 'renders nav' do
+      render
+      expect(rendered).to match(%{id="app-nav"})
+    end
+
+    it 'renders tabs' do
+      render
+      expect(rendered).to match(%{id="app-tabs"})
+    end
+
+    it 'yields' do
+      render plain: 'foobar', layout: 'layouts/application'
+      expect(rendered).to match(%{foobar})
+    end
+  end
+end
