c8032012b92ee23ce7adfc902ca038f1da6b15ab
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-29 18:22:58 -0500

    Update _nav partial

--- spec/views/layouts/_nav.html.slim_spec.rb
+++ spec/views/layouts/_nav.html.slim_spec.rb
@@ -1,32 +1,36 @@
 require 'rails_helper'
 
 describe 'layouts/_nav' do
+  subject { rendered }
+
   before do
     render
   end
 
-  it 'renders wrapper' do
-    expect(rendered).to match(%{nav class="navbar navbar-expand-md navbar-dark bg-dark"})
+  describe 'wrapper' do
+    it { is_expected.to have_css('nav.navbar.navbar-expand-md.navbar-dark.bg-dark') }
+    it { is_expected.to have_css('nav#app-nav') }
   end
 
-  context 'when user logged in' do
-    it 'renders brand link to terminals path' do
-      allow(view).to receive(:user_signed_in?).and_return(true)
-      render
-      expect(rendered).to match(%{<a class="navbar-brand" href="/terminals">Spectre</a>})
+  describe 'brand link' do
+    context 'when user logged in' do
+      it 'renders link to terminals path' do
+        allow(view).to receive(:user_signed_in?).and_return(true)
+        render
+        expect(rendered).to have_link('Spectre', href: '/terminals', class: 'navbar-brand')
+      end
+    end
+
+    context 'when user logged out' do
+      it 'renders link to root path' do
+        allow(view).to receive(:user_signed_in?).and_return(false)
+        render
+        expect(rendered).to have_link('Spectre', href: '/', class: 'navbar-brand')
+      end
     end
   end
 
-  context 'when user logged out' do
-    it 'renders brand link to root path' do
-      allow(view).to receive(:user_signed_in?).and_return(false)
-      render
-      expect(rendered).to match(%{<a class="navbar-brand" href="/">Spectre</a>})
-    end
-  end
-
-  it 'renders user links' do
-    render
-    expect(rendered).to match(%{id="user-links"})
+  describe 'user links' do
+    it { is_expected.to have_css('div#user-links') }
   end
 end
