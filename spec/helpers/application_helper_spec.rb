9c516baeea25a2e759ce3e28cb584bcf9f710798
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-30 13:03:43 -0500

    Refactor flash messages

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,65 +3,60 @@ require 'rails_helper'
 describe ApplicationHelper do
   describe '#flash_message' do
     context 'when has one' do
-      it 'returns wrapper' do
+      subject do
         allow(helper).to receive(:flash).and_return({ notice: 'foobar' })
-        expected = %{class="flash-message alert alert-success alert-dismissable fade show my-0"}
-        expect(helper.flash_message).to match(expected)
+        helper.flash_message
       end
 
-      it 'includes flash content' do
-        allow(helper).to receive(:flash).and_return({ notice: 'foobar' })
-        expect(helper.flash_message).to match(%{foobar})
-      end
-
-      it 'includes alert type' do
-        allow(helper).to receive(:flash).and_return({ foobar: 'foobar' })
-        expect(helper.flash_message).to match(%{alert-foobar})
-      end
+      it { is_expected.to have_css('.flash-message.alert.alert-success.alert-dismissable.fade.show.my-0') }
+      it { is_expected.to have_content('foobar') }
     end
 
-    context 'when flash is empty' do
+    context 'when is empty' do
       it 'returns empty string' do
         expect(helper.flash_message).to eq('')
       end
     end
 
     context 'when has two or more' do
-      it 'returns stacked alerts' do
+      subject do
         allow(helper).to receive(:flash).and_return({ notice: 'foobar', danger: 'foobaz' })
-        content = helper.flash_message
-        expect(content).to match(%{alert-success})
-        expect(content).to match(%{foobar})
-        expect(content).to match(%{alert-danger})
-        expect(content).to match(%{foobaz})
+        helper.flash_message
       end
+
+      it { is_expected.to have_css('.alert-success') }
+      it { is_expected.to have_content('foobar') }
+      it { is_expected.to have_css('.alert-danger') }
+      it { is_expected.to have_content('foobaz') }
     end
 
-    context 'when col is specified' do
-      it 'returns alert with col wrapper' do
-        allow(helper).to receive(:flash).and_return({ notice: 'foobar' })
-        expect(helper.flash_message('col-10')).to match(%{class="col-10"})
+    describe 'styling' do
+      context 'when col is specified' do
+        it 'returns alert with col wrapper' do
+          allow(helper).to receive(:flash).and_return({ notice: 'foobar' })
+          expect(helper.flash_message('col-10')).to have_css('.col-10')
+        end
       end
-    end
 
-    context 'when type is alert' do
-      it 'returns alert-danger' do
-        allow(helper).to receive(:flash).and_return({ alert: 'foobaz' })
-        expect(helper.flash_message).to match(%{alert-danger})
+      context 'when type is alert' do
+        it 'returns alert-danger' do
+          allow(helper).to receive(:flash).and_return({ alert: 'foobaz' })
+          expect(helper.flash_message).to have_css('.alert-danger')
+        end
       end
-    end
 
-    context 'when type is notice' do
-      it 'returns alert-success' do
-        allow(helper).to receive(:flash).and_return({ notice: 'foobaz' })
-        expect(helper.flash_message).to match(%{alert-success})
+      context 'when type is notice' do
+        it 'returns alert-success' do
+          allow(helper).to receive(:flash).and_return({ notice: 'foobaz' })
+          expect(helper.flash_message).to have_css('.alert-success')
+        end
       end
-    end
 
-    context 'when type is any other' do
-      it 'returs given type' do
-        allow(helper).to receive(:flash).and_return({ foo: 'bar' })
-        expect(helper.flash_message).to match(%{alert-foo})
+      context 'when type is any other' do
+        it 'returs given type' do
+          allow(helper).to receive(:flash).and_return({ foo: 'bar' })
+          expect(helper.flash_message).to have_css('.alert-foo')
+        end
       end
     end
   end
