ec77e5e43269d34dfeb18d39a58df523936dc350
Diego Lopez Garcia <aud0l@hotmail.com>
2017-08-29 19:20:51 -0500

    Update user links tests

--- spec/views/layouts/_user_links.html.slim_spec.rb
+++ spec/views/layouts/_user_links.html.slim_spec.rb
@@ -1,33 +1,38 @@
 require 'rails_helper'
 
 describe 'layouts/_user_links' do
-  context 'when user logged in' do
+  subject { rendered }
+
+  describe 'wrapper' do
     before do
-      allow(view).to receive(:user_signed_in?).and_return(true)
+      render
     end
 
-    it 'renders sign out link' do
-      render
-      expect(rendered).to match(%{data-method="delete" href="/user/sign_out"})
-      expect(rendered).to match(%{Sign Out})
+    it { is_expected.to have_css('div#user-links') }
+  end
+
+  describe 'items' do
+    context 'when logged in' do
+      before do
+        allow(view).to receive(:user_signed_in?).and_return(true)
+        render
+      end
+
+      it { is_expected.to have_link('Sign Out', href: '/user/sign_out') }
+      it { is_expected.to have_css('a[data-method="delete"]', text: 'Sign Out') }
+      it { is_expected.not_to have_link('Sign In') }
+      it { is_expected.not_to have_link('Sign Up') }
     end
   end
 
   context 'when user logged out' do
     before do
       allow(view).to receive(:user_signed_in?).and_return(false)
+      render
     end
 
-    it 'renders sign in link' do
-      render
-      expect(rendered).to match(%{href="/user/sign_in"})
-      expect(rendered).to match(%{Sign In})
-    end
-
-    it 'render sign up link' do
-      render
-      expect(rendered).to match(%{href="/user/sign_up"})
-      expect(rendered).to match(%{Sign Up})
-    end
+    it { is_expected.to have_link('Sign In', href: '/user/sign_in') }
+    it { is_expected.to have_link('Sign Up', href: '/user/sign_up') }
+    it { is_expected.not_to have_link('Sign Out') }
   end
 end
