4f680d5e800097f86661de46a99247a5f9217248
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-01 15:37:56 -0500

    Add tests to application decorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,12 +1,8 @@
 class ApplicationDecorator < Draper::Decorator
-  # Define methods for all decorated objects.
-  # Helpers are accessed through `helpers` (aka `h`). For example:
-  #
-  #   def percent_amount
-  #     h.number_to_percentage object.amount, precision: 2
-  #   end
 
   def handle_present(value, fallback = 'no given')
+    raise Exception.new('Value must exists') if value.nil?
+
     if value.present?
       return yield if block_given?
       value
@@ -14,4 +10,5 @@ class ApplicationDecorator < Draper::Decorator
       fallback
     end
   end
+
 end
