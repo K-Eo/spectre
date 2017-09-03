ff46c9bdfb4fb09562e366afe92e34359b7ba402
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-02 23:26:13 -0500

    Add dashboard controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   skip_before_action :set_company
 
   def index
+    if user_signed_in?
+      redirect_to dashboard_path
+    end
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 describe PagesController do
   describe 'GET index' do
-    it 'renders' do
-      get :index
-      expect(response).to have_http_status(200)
-      expect(response).to render_template('pages/index')
+    context 'when logged in' do
+      it 'redirects to dashboard' do
+        sign_in create(:user)
+        get :index
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+    context 'when logged out' do
+      it 'renders' do
+        get :index
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('pages/index')
+      end
     end
   end
 end
