2e5cbf80b5c8bab930bf0a648591223cfcb7f269
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-07 13:53:59 -0500

    Fix devise view tests

--- spec/views/layouts/_devise_nav.html.slim_spec.rb
+++ spec/views/layouts/_devise_nav.html.slim_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 describe 'layouts/_devise_nav' do
-  subject { rendered }
-
-  before do
+  subject {
+    stub_template 'shared/_user_links' => '<div id="user-links">User Links</div>'
     render
-  end
+  }
 
-  context 'navbar' do
+  describe 'navbar' do
     it { is_expected.to have_css('.navbar.navbar-expand-md.navbar-dark.px-0') }
     it { is_expected.to have_css('nav#app-nav') }
   end
 
-  context 'navbar brand' do
+  describe 'navbar brand' do
     it { is_expected.to have_css('a.navbar-brand') }
     it { is_expected.to have_link('Spectre', href: '/') }
   end
 
-  context 'user links' do
+  describe 'user links' do
     it { is_expected.to have_css('div#user-links') }
   end
 end
