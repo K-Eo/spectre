7e74e5c4e0783a67e0845f0d3db1bfc7a9071a5e
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-15 21:58:17 -0500

    Fix workers mailer preview and content

--- test/mailers/previews/workers_mailer_preview.rb
+++ test/mailers/previews/workers_mailer_preview.rb
@@ -3,7 +3,7 @@ class WorkersMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/workers/credentials
   def credentials
-    WorkersMailer.credentials(WorkerForm.new(email: 'foo@bar.com'))
+    WorkersMailer.credentials('foo@bar.com', 'worker password')
   end
 
 end
