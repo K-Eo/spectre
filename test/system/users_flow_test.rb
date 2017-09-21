0cfe7997f60638d29b52bce69cb54cb0aba3b8b3
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-21 13:28:27 -0500

    Fix users flow test

--- test/system/users_flow_test.rb
+++ test/system/users_flow_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class WorkersFlowTest < ApplicationSystemTestCase
+class UsersFlowTest < ApplicationSystemTestCase
   test "user adds new worker" do
     user = users(:jo)
 
@@ -10,18 +10,16 @@ class WorkersFlowTest < ApplicationSystemTestCase
     fill_in 'Password', with: 'password'
     click_button 'Log in'
 
-    visit workers_path
+    visit new_user_path
 
-    fill_in 'worker_email', with: 'foo@bar.com'
-    click_button 'Agregar trabajador'
+    fill_in 'user_email', with: 'foo@bar.com'
+    click_button 'Agregar Usuario'
 
     assert_text "Email sent to foo@bar.com."
 
     assert_text user.name, count: 1
     assert_no_text user.email
-    assert_text 'foo@bar.com', count: 2
-
-    assert_selector 'li[id^=worker_]', count: 1
+    assert_text 'foo@bar.com', count: 1
 
     assert_equal ['foo@bar.com'], last_email.to
     assert_equal 'Credenciales para cuenta de Spectre', last_email.subject
