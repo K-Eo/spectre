abc907846fe467b3f57ef29d5ff540fa20cb511a
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-22 17:05:47 -0500

    Rename system users flow test

--- test/system/add_user_test.rb
+++ test/system/add_user_test.rb
@@ -1,14 +1,9 @@
 require "application_system_test_case"
 
-class UsersFlowTest < ApplicationSystemTestCase
-  test "user adds new worker" do
+class AddUserTest < ApplicationSystemTestCase
+  test "user adds new user to company" do
     user = users(:eo)
-
-    visit new_user_session_path
-
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Log in'
+    sign(user)
 
     visit new_user_path
 
@@ -16,7 +11,6 @@ class UsersFlowTest < ApplicationSystemTestCase
     click_button 'Agregar Usuario'
 
     assert_text "Email sent to foo@bar.com."
-
     assert_text user.name, count: 1
     assert_no_text user.email
     assert_text 'foo@bar.com', count: 1
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -40,4 +40,11 @@ class ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  def sign(user, password = 'password')
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: password
+    click_button 'Log in'
+  end
+
 end
