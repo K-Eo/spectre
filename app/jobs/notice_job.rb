f8bd0e8903eaf903d377e0de2907278a41ffb66c
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-29 13:27:37 -0500

    Add critical queue to send notices to guard on alert

--- app/jobs/notice_job.rb
+++ app/jobs/notice_job.rb
@@ -1,6 +1,8 @@
 class NoticeJob < ApplicationJob
-  queue_as :default
+  queue_as :critical
 
   def perform(guard_id, alert_id)
+    alert = Alert.find(alert_id)
+    Pusher.trigger("private-#{guard_id}", "notice", guard: guard_id, alert: alert)
   end
 end
