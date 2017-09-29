13e65e927460d8e0fe3a0a49da123a49eb9f63d9
Diego Lopez Garcia <aud0l@hotmail.com>
2017-09-29 13:38:42 -0500

    Update integration test for send alerts

--- test/integration/send_alert_test.rb
+++ test/integration/send_alert_test.rb
@@ -5,7 +5,7 @@ class SendAlertTest < ActionDispatch::IntegrationTest
 
   test "user can send alerts to guards" do
     user = users(:eo)
-    params = { alert: { text: "foobar" } }
+    params = { alert: { text: "send_alert_test" } }
 
     assert_enqueued_jobs 0
 
@@ -16,5 +16,12 @@ class SendAlertTest < ActionDispatch::IntegrationTest
     assert_enqueued_jobs 3
 
     assert_response :created
+
+    alert = Alert.find_by(text: 'send_alert_test')
+
+    assert_equal alert.guards.count, 3
+    assert alert.has_guard?(users(:mia))
+    assert alert.has_guard?(users(:lee))
+    assert alert.has_guard?(users(:tom))
   end
 end
