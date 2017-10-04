5eff9090a6fa5d3612bcbfbf4e702a3b35201533
Diego Lopez Garcia <aud0l@hotmail.com>
2017-10-04 16:07:27 -0500

    Update circleci config file

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,15 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
-    browser: :remote,
-    desired_capabilities: :chrome
-  }
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   # driven_by :poltergeist
 
-  def setup
-    Capybara.app_host = 'http://172.17.0.1'
-  end
+  # def setup
+  #   Capybara.app_host = 'http://172.17.0.1'
+  # end
 
   def login_as(user, password = "password")
     email = user.respond_to?(:email) ? user.email : user
